6127ef991779c522b91560140a4c289cb12b68fc
yunoi <yunoek@gmail.com>
2020-02-15 17:36:04 +0900

    강윤해 - db 연결 문제 수정, 메인 페이지 css 교체중

--- common/lib/main.php
+++ common/lib/main.php
@@ -1,5 +1,31 @@
 
 <div id="main_content">
+<div id="carousel_section">
+<ul>
+    <li>
+        <img src="http://<?php echo $_SERVER['HTTP_HOST']; ?>/helf/common/img/img01.png">
+    </li>
+    <li>
+        <img src="http://<?php echo $_SERVER['HTTP_HOST']; ?>/helf/common/img/img02.png">
+    </li>
+    <li>
+        <img src="http://<?php echo $_SERVER['HTTP_HOST']; ?>/helf/common/img/img03.png">
+    </li>
+    <li>
+        <img src="http://<?php echo $_SERVER['HTTP_HOST']; ?>/helf/common/img/img04.png">
+    </li>
+</ul>
+<div id="slideshow_nav">
+    <a href="#" class="prev">prev</a>
+    <a href="#" class="next">next</a>
+</div>
+<div id="slideshow_indi">
+    <a href="#" class='active'>&nbsp;&nbsp;</a>
+    <a href="#">&nbsp;&nbsp;</a>
+    <a href="#">&nbsp;&nbsp;</a>
+    <a href="#">&nbsp;&nbsp;</a>
+</div>
+</div>
   <div id=board_preview>
 <div id="latest">
     <h4>최근 게시글</h4>

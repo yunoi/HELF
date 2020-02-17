305e7e927d3ef223cf780e2c203ea26be24ccbe3
grace-sammy <ysm5052@gmail.com>
2020-02-17 09:21:02 +0900

    유세미 - 자유게시판 파일 업로드 및 댓글, 삭제 기능중

--- community/free/lib/file_upload.php
+++ community/free/lib/file_upload.php
@@ -7,9 +7,8 @@ $upfile_tmp_name= $_FILES['upfile']['tmp_name'];
 $upfile_error= $_FILES['upfile']['error'];
 $upfile_size= $_FILES['upfile']['size'];
 
-echo "<script>alert('upfile_name:{$upfile_name}');</script>";
-
-//2. 파일명과 확장자를 구분해서 저장한다.
+if ($upfile_name && !$upfile_error) {
+    //2. 파일명과 확장자를 구분해서 저장한다.
 $file = explode(".", $upfile_name); //파일명과 확장자구분에서 배열저장
 $file_name = $file[0];              //파일명
 $file_extension = $file[1];         //확장자
@@ -18,41 +17,41 @@ $file_extension = $file[1];         //확장자
 $upload_dir ="./data/"; //업로드된파일을 저장하는장소지정
 
 //4.파일업로드가성공되었는지 점검한다. 성공:0 실패:1
-//파일명이 중복되지 않도록 임의파일명을 정한다.
-if(!$upfile_error){
-  $new_file_name=date("Y_m_d_H_i_s");
-  $new_file_name = $new_file_name."_"."0";
-  $copied_file_name= $new_file_name.".".$file_extension;
-  $uploaded_file = $upload_dir.$copied_file_name;
-  // $uploaded_file = "./data/2019_04_22_15_09_30_0.jpg";
-}
+    //파일명이 중복되지 않도록 임의파일명을 정한다.
+    if (!$upfile_error) {
+        $new_file_name=date("Y_m_d_H_i_s");
+        $new_file_name = $new_file_name."_"."0";
+        $copied_file_name= $new_file_name.".".$file_extension;
+        $uploaded_file = $upload_dir.$copied_file_name;
+        // $uploaded_file = "./data/2019_04_22_15_09_30_0.jpg";
+    }
 
-//5 업로드된 파일확장자를 체크한다.  "image/gif"
-$type=explode("/", $upfile_type);
+    //5 업로드된 파일확장자를 체크한다.  "image/gif"
+    $type=explode("/", $upfile_type);
 
-if($type[0]=='image'){
-  switch ($type[1]) {
+    if ($type[0]=='image') {
+        switch ($type[1]) {
     case 'gif': case 'jpg': case 'png': case 'jpeg':
       case 'pjpeg': break;
       default:alert_back('3. gif jpg png 확장자가아닙니다.');
     }
-    //6 업로드된 파일사이즈(2mb)를 체크해서 넘어버리면 돌려보낸다.
-    if($upfile_size>2000000){
-      alert_back('2. 이미지파일사이즈가 2MB이상입니다.');
+        //6 업로드된 파일사이즈(2mb)를 체크해서 넘어버리면 돌려보낸다.
+        if ($upfile_size>2000000) {
+            alert_back('2. 이미지파일사이즈가 2MB이상입니다.');
+        }
+    } else {
+        //5 업로드된 파일사이즈(500kb)를 체크해서 넘어버리면 돌려보낸다.
+        if ($upfile_size>500000) {
+            alert_back('2. 파일사이즈가 500KB이상입니다.');
+        }
     }
-}else{
-    //5 업로드된 파일사이즈(500kb)를 체크해서 넘어버리면 돌려보낸다.
-    if($upfile_size>500000){
-        alert_back('2. 파일사이즈가 500KB이상입니다.');
+
+    //7. 임시저장소에 있는 파일을 서버에 지정한 위치로 이동한다.
+    if (!move_uploaded_file($upfile_tmp_name, $uploaded_file)) {
+        alert_back('4. 서버 전송에러!!');
     }
+} else {
+    $upfile_name="";
+    $upfile_type="";
+    $copied_file_name="";
 }
-
-//7. 임시저장소에 있는 파일을 서버에 지정한 위치로 이동한다.
-if(!move_uploaded_file($upfile_tmp_name, $uploaded_file)){
-  echo "<script>alert('{$upfile_tmp_name}');</script>";
-  echo "<script>alert('{$uploaded_file}');</script>";
-  alert_back('4. 서버 전송에러!!');
-}
-
-
-?>

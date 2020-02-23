e362852a7268ead5023afccb1930fd9ee698e32d
yunoi <yunoek@gmail.com>
2020-02-23 12:48:16 +0900

    강윤해 - 메시지 폼 작업중

--- message/js/message.js
+++ message/js/message.js
@@ -2,16 +2,16 @@
 $(document).ready(function() {
   $("#send_message").click(function() {
         location.replace('./message_box.php?mode=send');
-        $("#send_message").addClass("select_tap");
-        $("#receive_message").removeClass("select_tap");
+        $("#send_message").toggleClass("select_tap");
+        $("#receive_message").toggleClass("select_tap");
       
       
   });
 
   $("#receive_message").click(function() {
     location.replace('./message_box.php?mode=receive');
-    $("#receive_message").addClass("select_tap");
-    $("#send_message").removeClass("select_tap");
+    $("#receive_message").toggleClass("select_tap");
+    $("#send_message").toggleClass("select_tap");
     
   });
 
--- message/message_form.php
+++ message/message_form.php
@@ -1,19 +1,24 @@
+<?php
+session_start();
+if(isset($_SESSION["user_id"])){
+  $user_id = $_SESSION["user_id"];
+} else {
+  $user_id = "";
+}
+?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>메시지</title>
+    <title>메시지 보내기</title>
     <link rel="stylesheet" type="text/css" href="./css/common.css">
     <link rel="stylesheet" type="text/css" href="./css/message.css">
     <link rel="shortcut icon" href="./favicon.ico">
     <script src="./js/message.js"></script>
   </head>
   <body>
-    <header>
-        <?php include "header.php";?>
-    </header>
     <?php
-      if(!$userId){
+      if(!$user_id){
         echo ("<script>
           alert('로그인 후 이용해 주세요!');
           history.go(-1);
@@ -22,26 +27,22 @@
       }
     ?>
     <section>
-      <div id="message_img_bar">
-        <a href="#"><img id="img_title" src="./img/ham_message.png" alt="햄스터애호가클럽 메시지폼 이미지"></a>
-      </div>
-      <!-- id get방식 말고 input hidden으로 보내기!! -->
       <div id="message_box">
-        <h3 id="write_title">쪽지 보내기</h3>
+        <h3 id="write_title">메시지 보내기</h3>
         <ul class="top_buttons">
-          <li><a href="message_box.php?mode=receive">수신 쪽지함</a></li>
-          <li><a href="message_box.php?mode=send">송신 쪽지함</a></li>
+          <li><a href="message_box.php?mode=receive">받은 메시지</a></li>
+          <li><a href="message_box.php?mode=send">보낸 메시지</a></li>
         </ul>
         <form name="message_form" action="message_insert.php" method="post">
           <div id="write_msg">
             <ul>
               <li>
                 <span class="col1">보내는 사람: </span>
-                <span class="col2"><?=$userId?></span>
-                <input type="hidden" name="send_id" value="<?=$userId?>">
+                <span class="col2"><?=$user_id?></span>
+                <input type="hidden" name="send_id" value="<?=$user_id?>">
               </li>
               <li>
-                <span class="col1">수신 아이디: </span>
+                <span class="col1">받는 사람 <br>아이디: </span>
                 <span class="col2"><input type="text" name="rv_id" value=""></span>
               </li>
               <li>
@@ -58,8 +59,5 @@
         </form>
       </div>
     </section>
-    <footer>
-        <?php include "footer.php";?>
-    </footer>
   </body>
 </html>

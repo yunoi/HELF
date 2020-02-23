562d2627201531486726f8366938630173ff8638
neul713 <neul713@naver.com>
2020-02-23 15:52:04 +0900

    김하늘 - 마이페이지 수정, 헤더 수정

--- common/lib/header.php
+++ common/lib/header.php
@@ -56,7 +56,7 @@
     $logged = $user_name."(".$user_id.") 님";
 ?>
       <li><a href="#"><img src="http://<?php echo $_SERVER['HTTP_HOST'].$envelope;?>" alt="쪽지함" onclick="message_box()"></a>
-          <span><a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/helf/mypage/mypage_info.php"><?=$logged?></a> </span></li>
+          <span><a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/helf/mypage/mypage_board.php"><?=$logged?></a> </span></li>
       <li> | </li>
       <li>
         <a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/helf/member/member_form.php?modify=modify">내 정보 수정</a>

7c4fef0e9a6fa610b39597071b8aad88e9c09ab6
veripers43 <personave@naver.com>
2020-02-27 03:46:37 +0900

    김판규 - 프로그램 리스트 검색, 정렬

--- program/pick_db.php
+++ program/pick_db.php
@@ -8,6 +8,14 @@ if (!$conn) {
     exit();
 }
 
+if(isset($_SESSION["user_id"])){
+  $user_id = $_SESSION["user_id"];
+} else {
+  echo "<script>alert('로그인 후 이용해주세요^오^')</script>";
+  echo "<script>history.go(-1);</script>";
+  exit();
+}
+
 if (isset($_GET["o_key"])){
   $o_key = $_GET["o_key"];
 }else{
@@ -20,25 +28,43 @@ if (isset($_GET["shop"])){
   $shop = "";
 }
 
-if(isset($_SESSION["user_id"])){
-  $user_id = $_SESSION["user_id"];
-} else {
-  echo "<script>alert('로그인 후 이용해주세요^오^')</script>";
-  echo "<script>history.go(-1);</script>";
-  exit();
+if (isset($_GET["mode"])){
+  $mode = $_GET["mode"];
+}else{
+  $mode = "";
 }
 
+function pick_insert($conn, $user_id, $o_key, $shop){
 
+  $sql = "insert into pick values (null,'$user_id','$o_key')";
 
-$sql = "insert into pick values (null,'$user_id','$o_key')";
+  mysqli_query($conn, $sql);
+  mysqli_close($conn);
 
-mysqli_query($conn, $sql);
-mysqli_close($conn);
+  echo "<script>alert('$shop'+' 찜 목록에 추가완료!');</script>";
+  echo "<script>document.location.href='./program.php';</script>";
 
-echo "<script>alert('$shop'+' 찜 목록에 추가완료!');</script>";
-echo "<script>document.location.href='./program.php';</script>";
+}
 
+function pick_delete($conn, $user_id, $o_key, $shop){
 
+  $sql = "delete from pick where id ='$user_id' and o_key ='$o_key'";
 
+  mysqli_query($conn, $sql);
+  mysqli_close($conn);
+
+  echo "<script>alert('$shop'+' 찜 삭제완료!');</script>";
+  echo "<script>document.location.href='./program.php';</script>";
+
+}
+
+switch ($mode) {
+  case 'insert':
+    pick_insert($conn, $user_id, $o_key, $shop);
+    break;
+  case 'delete':
+    pick_delete($conn, $user_id, $o_key, $shop);
+    break;
+}
 
 ?>

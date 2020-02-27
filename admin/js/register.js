a06728e845e488c78796437c4e85c4bcaee89a66
veripers43 <personave@naver.com>
2020-02-27 13:52:29 +0900

    김판규 - 프로그램 옵션추가

--- admin/js/register.js
+++ admin/js/register.js
@@ -4,4 +4,23 @@ $(document).ready(function() {
 
   });
 
+  $("#option_plus").click(function(){
+    $.ajax({
+      url:'',
+      type:'',
+      // data:{'list':count},
+      success:function(data){
+
+
+
+
+      },
+      error:function(){
+        var html = `<br><input type="text" name="choose" value="">`;
+        $("#td_plus").append(html);
+      }
+    });
+
+  })
+
 });

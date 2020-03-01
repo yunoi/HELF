b6ea12053cac01d4299afb2c0ef3bab8c5815157
yunoi <yunoek@gmail.com>
2020-03-01 17:22:45 +0900

    강윤해 - 공지사항, faq 버튼 수정

--- FAQ/view.php
+++ FAQ/view.php
@@ -74,21 +74,22 @@ session_start();
 
             <div id="write_button">
               <!--목록보기 -->
-              <a href="./list.php?page=<?=$page?>">목록</a>&nbsp;
+              
+              <button type="button"><a href="./list.php?page=<?=$page?>">목록</a></button>&nbsp;
 
             <?php
               //세션값이 존재하면 수정기능과 삭제기능부여하기
               if(isset($_SESSION['user_id'])){
                 if($_SESSION['user_grade']=="admin"){
-                  echo('<a href="./write_edit_form.php?mode=update&num='.$num.'">수정</a>&nbsp;&nbsp;');
-                  echo('<button type="button" id="write_button_delete" onclick="check_delete('.$num.')">삭제</button>&nbsp&nbsp;');
+                  echo('<button type="button"><a href="./write_edit_form.php?mode=update&num='.$num.'">수정</a></button>&nbsp;&nbsp;');
+                  echo('<button type="button" id="write_button_delete" onclick="check_delete('.$num.')"><a>삭제</a></button>&nbsp');
                   
                 }
               }
               // 세션값이 존재하면 글쓰기 기능부여하기\
               if(isset($_SESSION['user_grade'])){
               if($_SESSION['user_grade']=="admin"){
-                echo '<a href="write_edit_form.php">글쓰기</a>';
+                echo ' <button type="button"><a href="write_edit_form.php">글쓰기</a></button>';
               }
             }
             ?>
